a04d7b80a0c3d63dc782b24c7830f692a4fae36f
liixini <liixini@users.noreply.github.com>
2026-09-12 12:13:55 +0200

    Remove custom cache trust from the Nix flake (#34)

--- flake.nix
+++ flake.nix
@@ -3,15 +3,6 @@
 
   inputs.release.url = "github:liixini/skwd-wall/nix";
 
-  nixConfig = {
-    extra-substituters = [
-      "https://github.com/liixini/skwd-wall/releases/download/nix-cache"
-    ];
-    extra-trusted-public-keys = [
-      "skwd-wall-v2-1:oDTVsejeUw3NmND4qKUwek0muuUnsolw/5qM8n/Gl54="
-    ];
-  };
-
   outputs = { release, ... }: {
     packages = release.packages;
     checks = release.checks;
